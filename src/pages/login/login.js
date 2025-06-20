5ee07280efb950a49afc84fabb4d13e9fddb4e08
Jay Santana <jayannyrayara@gmail.com>
2025-06-20 14:04:29 -0300

    T001: feat(login) - implementa habilitação do botão, captura do nome e redirecionamento para página do jogo

---
+++ src/pages/login/login.js
@@ -0,0 +1,22 @@
+const input = document.querySelector('.login-input');
+const playButton = document.querySelector('.login-button');
+const form = document.querySelector('.login-form');
+
+
+const validateInput = ({target}) => {
+  if(target.value.length > 2) {
+    playButton.removeAttribute('disabled');
+    return
+  }
+  playButton.setAttribute('disabled', '');
+}
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  localStorage.setItem('player', input.value);
+  window.location = '../game/game.html';
+
+}
+
+input.addEventListener('input', validateInput);
+form.addEventListener('submit', handleSubmit);
